82b9c40406f48fe0c9f03bac836f5d227f4fc407
KaechiKensuke <CA1254005@st.kawahara.ac.jp>
2025-08-04 10:35:32 +0900

    <Enemy>玉の発射

---
+++ Assets/Script/BulletManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int bulletSpeed = 10;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    private void Move()
+    {
+        transform.position += new Vector3(0, 0,-bulletSpeed) * Time.deltaTime; 
+    }
+}
--- Assets/Script/EnemyManager.cs
+++ Assets/Script/EnemyManager.cs
@@ -6,14 +6,28 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private float moveSpeed = 10.0f;
 
+    public GameObject bulletPrefab;
+    public GameObject firingPosition;
+    
+    //Enemy‚ª¶¬‚³‚ê‚½‚ç’e‚ğ”­Ë‚·‚é‚æ‚¤‚É‚·‚é
+    private void Start()
+    {
+        Shot();
+    }
+
     // Update is called once per frame
     void Update()
     {
         Move();
     }
-    //Enemy‚ğ¶‚É“®‚©‚·
+    //Bullet‚ğ¶‚É“®‚©‚·
     private void Move()
     {
         transform.position += new Vector3(0, 0, -moveSpeed) * Time.deltaTime;
+    }
+
+    private void Shot()
+    {
+        Instantiate(bulletPrefab,firingPosition.transform.transform.position,transform.rotation)
     }
 }
