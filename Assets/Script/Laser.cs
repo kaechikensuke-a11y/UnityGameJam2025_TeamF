a041a1dc1f4acfcc2f0ba05a7c74e5067c8ef871
kaechikensuke-a11y <kaechikensuke@gmail.com>
2025-08-04 11:44:08 +0900

    Kaechi/create player (#7)
    Co-authored-by: KaechiKensuke <CA1254005@st.kawahara.ac.jp>

---
+++ Assets/Script/Laser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private float bulletSpeed = 100.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    //ƒoƒŒƒbƒg‚ð”ò‚Î‚·
+    private void Move()
+    {
+        transform.position += new Vector3(0,0,bulletSpeed) * Time.deltaTime;
+    }
+}
